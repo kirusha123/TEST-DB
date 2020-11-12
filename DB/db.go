bb56811f283fd378a5c076514679529a4147a106
kirusha123 <kirriess@mail.ru>
2020-11-12 15:47:57 +0300

    test

--- DB/db.go
+++ DB/db.go
@@ -1,11 +1,33 @@
-package database
+package DB
 
-type DB struct {
+//DBconf . . .
+type DBconf struct {
+	host   string
+	port   int
+	user   string
+	pass   string
+	dbname string
+}
+
+//NewDBconfig config for DB
+func NewDBconfig() *DBconf {
+	return &DBconf{
+		host:   "localhost",
+		port:   5432,
+		user:   "postgres",
+		pass:   "admin",
+		dbname: "PSQLtest",
+	}
+
+}
+
+type database struct {
 	cfg *DBconf
 }
 
-func NewDB(config *DBconf) *DB {
-	return &DB{
+//NewDB sad...
+func NewDB(config *DBconf) *database {
+	return &database{
 		cfg: config,
 	}
 }
